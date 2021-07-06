c4bf8627997af4a098c858f440f07b7717631738
Jonas Passweg <jpassweg@student.ethz.ch>
2021-07-06 15:58:12 +0200

    commented last edit

--- cil_data/mask_to_submission.py
+++ cil_data/mask_to_submission.py
@@ -9,6 +9,7 @@ foreground_threshold = 0.25 # percentage of pixels > 1 required to assign a fore
 
 # assign a label to a patch
 def patch_to_label(patch):
+    # changed from np.mean since now, we also use this function in our loss function
     df = torch.mean(patch)
     if df > foreground_threshold:
         return 1
