68c92ec0ae0c96eb6ba84c936193596186c533a5
Jonas Passweg <jpassweg@student.ethz.ch>
2021-07-09 13:30:26 +0200

    one node model

--- models/simple_models.py
+++ models/simple_models.py
@@ -19,3 +19,23 @@ class ZeroModel(nn.Module):
         else:
             return torch.zeros_like(x[:,0,...], dtype=torch.float, requires_grad=False)
         
+
+# multiplies every pixel with one value
+class OneNodeModel(nn.Module):
+    def __init__(self):
+        super(OneNodeModel, self).__init__()
+        # define single weight
+        self.weight = torch.randn((3, 1), dtype=torch.float, requires_grad=True)
+
+    # defines forward pass (never call yourself)
+    def forward(self, x):
+        batching = (len(x.shape) == 4)
+        # transpose to get the channels at last index
+        x_t = x.transpose(0, 2, 3, 1) if batching else x.transpose(1, 2, 0)
+        # multiply with weights
+        z = torch.matmul(x_t, self.weight)
+        # flatten last dimension
+        z_flat = torch.flatten(z, start_dim=2) if batching else torch.flatten(z, start_dim=1)
+        # relu round
+        z_relu = torch.nn.ReLU(z_flat)
+        return z_relu
