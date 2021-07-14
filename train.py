990aa2f2c9c93c5af9a07f70a2d992398e2ab161
Jonas Passweg <jpassweg@student.ethz.ch>
2021-07-14 19:34:18 +0200

    updated train.py

--- train.py
+++ train.py
@@ -18,7 +18,8 @@ sys.path.append("tools")
 import data
 import simple_models
 import loss_functions
-import reproducible as repr
+import reproducible
+from datawriter import datawriter
 
 # specify hyperparameters
 hyperparameters = {
@@ -33,7 +34,7 @@ hyperparameters = {
     "store_model": True,
 }
 
-if(hyperparameters["reproducible"]): repr.set_deterministic()
+if(hyperparameters["reproducible"]): reproducible.set_deterministic()
 
 
 """
@@ -51,20 +52,18 @@ def get_dataloader():
     dataset_size = len(dataset) # size of dataset needed to compute split
     train_split_size = int(dataset_size * hyperparameters["train_eval_ratio"])
     # split dataset in training and evaluation sets
-    train_dataset, eval_dataset = torch.utils.data.random_split(dataset, [
-        train_split_size, 
-        dataset_size - train_split_size
-        ])
+    train_dataset, eval_dataset = torch.utils.data.random_split(dataset, 
+        [train_split_size, dataset_size - train_split_size])
 
     # initiate dataloader for training and evaluation datasets
     train_dataloader = DataLoader(train_dataset, 
         batch_size=hyperparameters["batch_size"], shuffle=hyperparameters["shuffle"], 
-        worker_init_fn=repr.seed_worker if hyperparameters["reproducible"] else None, 
-        generator=repr.g if hyperparameters["reproducible"] else None)
+        worker_init_fn=reproducible.seed_worker if hyperparameters["reproducible"] else None, 
+        generator=reproducible.g if hyperparameters["reproducible"] else None)
     eval_dataloader = DataLoader(eval_dataset, 
         batch_size=1, shuffle=hyperparameters["shuffle"], 
-        worker_init_fn=repr.seed_worker if hyperparameters["reproducible"] else None, 
-        generator=repr.g if hyperparameters["reproducible"] else None)
+        worker_init_fn=reproducible.seed_worker if hyperparameters["reproducible"] else None, 
+        generator=reproducible.g if hyperparameters["reproducible"] else None)
 
     return train_dataloader, eval_dataloader
 
@@ -73,13 +72,10 @@ def get_dataloader():
 trains the model using one optimizer step for each batch in the dataloader
 """
 def train(dataloader, model, loss_fn, optimizer):
-    size = len(dataloader.dataset)
-    for batch_number, sample in enumerate(dataloader):
-        # Compute prediction and loss
+    for _, sample in enumerate(dataloader):
         pred = model(sample["image"])
         loss = loss_fn(pred, sample["ground_truth"])
 
-        # Backpropagation
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
@@ -89,36 +85,43 @@ def train(dataloader, model, loss_fn, optimizer):
 evaluates the model by computing the average loss over all batches in the data_loader
 """
 def evaluate(dataloader, model, loss_fn):
-    size = len(dataloader.dataset)
-    num_batches = len(dataloader)
-    test_loss = 0
+    eval_loss = 0
 
     with torch.no_grad():
         for sample in dataloader:
             pred = model(sample["image"])
-            test_loss += loss_fn(pred, sample["ground_truth"]).item()
+            eval_loss += torch.mean(loss_fn(pred, sample["ground_truth"])).item()
 
-    test_loss /= num_batches
-    return test_loss
+    eval = {
+        "eval_loss": eval_loss / len(dataloader),
+        "sample": sample,
+        "prediction": pred
+    }
+
+    return eval
 
 
 def main():
     # check if cuda available
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    print('Using {} device'.format(device))
+
+    # set up datawriter
+    dw = datawriter()
+    dw.write_info("Using {} device".format(device))
+    dw.write_hyperparameters(hyperparameters)
     
-    # get dataloaders
+    # set up dataloaders
     train_dataloader, eval_dataloader = get_dataloader()
     
     # choose model
     if(hyperparameters["load_model"] and os.path.exists("model.pth")):
-        print("Model loaded from 'model.pth'")
         model = torch.load('model.pth')
+        dw.write_info("Model loaded from 'model.pth'")
+        dw.set_model(model)
     else:
         model = simple_models.OneNodeModel().to(device)
+        dw.set_model(model)
     
-    print("Model used: {} \n" .format(model))
-
     # choose loss function
     loss_fn = torch.nn.MSELoss()
 
@@ -128,12 +131,10 @@ def main():
     # train
     for epoch in range(hyperparameters["epochs"]):
         train(train_dataloader, model, loss_fn, optimizer)
-        test_loss = evaluate(eval_dataloader, model, loss_fn)
-        print(f"Epoch {epoch+1:>4d} Eval avg loss: {test_loss:>8f}")
+        dw.write_eval(epoch, evaluate(eval_dataloader, model, loss_fn))
     
     # stores model such that it can be reloaded later
-    if(hyperparameters["store_model"]):
-        torch.save(model, 'model.pth')
+    if(hyperparameters["store_model"]): torch.save(model, 'model.pth')
 
 
 if __name__ == "__main__":
