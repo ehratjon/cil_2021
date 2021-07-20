702eb671159ff5f602aed541ac0387b831d28a81
Jonas Passweg <jpassweg@student.ethz.ch>
2021-07-20 10:42:24 +0200

    moved nb's to files

--- tools/data_tools.py
+++ tools/data_tools.py
@@ -1,6 +1,7 @@
 import os
 import torch
 from PIL import Image
+import torchvision.transforms.functional as F
 
 
 def store_tensor_as_image(tensor, path):
@@ -28,3 +29,36 @@ def image_to_tensor(image, float=True):
     if(float):
         image = image.float()
     return image
+
+
+# from mike
+# TODO: change from matplotlib to store file
+"""
+def show_image(imgs):
+    if not isinstance(imgs, list):
+        imgs = [imgs]
+    fix, axs = plt.subplots(ncols=len(imgs), squeeze=False)
+    for i, img in enumerate(imgs):
+        img = img.detach()
+        img = F.to_pil_image(img)
+        axs[0, i].imshow(np.asarray(img))
+        axs[0, i].set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
+"""
+
+# from mike to change color (as not to overfit model to color paletter)
+# TODO: fix
+"""
+def change_color(X, y):
+    Xs, ys = next(iter(system.val_dataloader()))
+
+    y_preds = torch.sigmoid(model(Xs.float()))
+
+    imgs_masks_zip = list(zip(Xs, y))
+    seg_imgs_masks = [draw_segmentation_masks(train_pair[0], train_pair[1].bool(), colors=['#FF0000']) for train_pair in imgs_masks_zip]
+
+    pred_zip = list(zip(seg_imgs_masks, y_preds))
+    seg_imgs_pred = [draw_segmentation_masks(train_pair[0], train_pair[1].round().bool(), colors=['#00ff00']) for train_pair in pred_zip]
+
+    for i, seg_image in enumerate(seg_imgs_pred):
+        show_image(seg_image)
+"""
---
+++ train_l3.py
@@ -0,0 +1,86 @@
+from data import RoadSatelliteModule
+from system import SemanticSegmentationSystem
+from model import NestedUNet
+from model import UNet
+
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+import torchvision
+import torchvision.transforms.functional as F
+
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import EarlyStopping
+
+import torch
+
+from torchinfo import summary
+
+# specify hyperparameters
+hyperparameters = {
+    "epochs": 5,
+    "learning_rate": 1e-2, 
+    # was reduced to 8 so that it can still fit on a gpu
+    "batch_size": 8, 
+    "shuffle": True,
+    # Recommended not to shuffle validation data loader
+    "shuffle_val": False, 
+    "train_eval_ratio": 0.9,
+    "num_workers": 24, # best to set to number of cpus on machine
+    # not really hyperparameters but used to set behaviour of model
+    "reproducible": True,
+    "load_model": False,
+    "store_model": True,
+    # choose if params/images are being stored in results file
+    # these parameters are given to datawriter.py
+    # info: stores all parameters in csv files
+    # info: stores last batch of evaluation images for each epoch
+    "write_params": True,
+    "write_images": True,
+}
+
+def main():
+    pl.seed_everything(42, workers=True)
+    
+    # prepare data
+    road_data = RoadSatelliteModule()
+    X, _ = next(iter(road_data.train_dataloader()))
+    
+    # set model and system
+    model = NestedUNet(1, 3).cuda()
+    summary(model, input_size=(X.shape))
+    system = SemanticSegmentationSystem(model, road_data)
+
+    # choose an early callback function
+    early_stop_callback = EarlyStopping(
+        monitor='validation_metric',
+        patience=20,
+        verbose=2,
+        mode='max'
+    )
+
+    # set up trainer
+    trainer = pl.Trainer(
+        #fast_dev_run=True,
+        gpus=-1,
+        auto_select_gpus=True,
+        #auto_lr_find=True,
+        auto_scale_batch_size='binsearch',
+        stochastic_weight_avg=True,
+        benchmark=True,
+        callbacks=[early_stop_callback]
+    )
+
+    # tune system (set parameters)
+    trainer.tune(system)
+
+    # fit system (actual training)
+    trainer.fit(system)
+
+    # visualize results
+    system.visualize_results()
+
+if __name__ == "__main__":
+    main()
+    print("Done!")
