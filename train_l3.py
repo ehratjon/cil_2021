49c49fcaf31efe426bcef2e04ef1d3c18e3e73f3
Jonas Passweg <jpassweg@student.ethz.ch>
2021-07-20 10:50:35 +0200

    last bugfixes

--- train_l3.py
+++ train_l3.py
@@ -1,44 +1,25 @@
-from data import RoadSatelliteModule
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+import sys
+
+import torchvision
+import torchvision.transforms.functional as F
+import torch
+from torchinfo import summary
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import EarlyStopping
+
+sys.path.append("cil_data")
+sys.path.append("data")
+sys.path.append("models")
+sys.path.append("tools")
+
+from data_l2 import RoadSatelliteModule
 from system_l2 import SemanticSegmentationSystem
 from model_l2 import NestedUNet
 from model_l2 import UNet
 
-import numpy as np
-import matplotlib.pyplot as plt
-import random
-
-import torchvision
-import torchvision.transforms.functional as F
-
-import pytorch_lightning as pl
-from pytorch_lightning.callbacks import EarlyStopping
-
-import torch
-
-from torchinfo import summary
-
-# specify hyperparameters
-hyperparameters = {
-    "epochs": 5,
-    "learning_rate": 1e-2, 
-    # was reduced to 8 so that it can still fit on a gpu
-    "batch_size": 8, 
-    "shuffle": True,
-    # Recommended not to shuffle validation data loader
-    "shuffle_val": False, 
-    "train_eval_ratio": 0.9,
-    "num_workers": 24, # best to set to number of cpus on machine
-    # not really hyperparameters but used to set behaviour of model
-    "reproducible": True,
-    "load_model": False,
-    "store_model": True,
-    # choose if params/images are being stored in results file
-    # these parameters are given to datawriter.py
-    # info: stores all parameters in csv files
-    # info: stores last batch of evaluation images for each epoch
-    "write_params": True,
-    "write_images": True,
-}
 
 def main():
     pl.seed_everything(42, workers=True)
@@ -62,7 +43,7 @@ def main():
 
     # set up trainer
     trainer = pl.Trainer(
-        #fast_dev_run=True,
+        fast_dev_run=True,
         gpus=-1,
         auto_select_gpus=True,
         #auto_lr_find=True,
