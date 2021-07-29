5ccf6e5d58ca1361351fae41d227d4b1ee352a9f
Mike Boss <mboss@lo-login-01.leonhard.ethz.ch>
2021-07-29 10:28:48 +0200

    Spatial stuff

--- models.py
+++ models.py
@@ -68,9 +68,7 @@ class UNet_3Plus_Spatial_Dilated(nn.Module):
 
         # h2->160*160, hd4->40*40, Pooling 4 times
         self.h2_PT_hd4 = nn.MaxPool2d(4, 4, ceil_mode=True)
-        self.h2_PT_hd4_spatial = ResSDNLayer(filters[1], self.CatChannels, 16, range(4), 3, 1, 1, True)
-        self.h2_PT_hd4_bn = nn.BatchNorm2d(self.CatChannels)
-        self.h2_PT_hd4_relu = nn.ReLU(inplace=True)
+        self.h2_PT_hd4_conv = dilated_block(filters[1], self.CatChannels)
 
         # h3->80*80, hd4->40*40, Pooling 2 times
         self.h3_PT_hd4 = nn.MaxPool2d(2, 2, ceil_mode=True)
@@ -84,7 +82,7 @@ class UNet_3Plus_Spatial_Dilated(nn.Module):
         self.hd5_UT_hd4_conv = dilated_block(filters[4], self.CatChannels)
 
         # fusion(h1_PT_hd4, h2_PT_hd4, h3_PT_hd4, h4_Cat_hd4, hd5_UT_hd4)
-        self.conv4d_1 = dilated_block(self.UpChannels, self.UpChannels)  # 16
+        self.conv4d_1 = dilated_spatial_block(self.UpChannels, self.UpChannels)  # 16
 
         '''stage 3d'''
         # h1->320*320, hd3->80*80, Pooling 4 times
@@ -93,9 +91,7 @@ class UNet_3Plus_Spatial_Dilated(nn.Module):
 
         # h2->160*160, hd3->80*80, Pooling 2 times
         self.h2_PT_hd3 = nn.MaxPool2d(2, 2, ceil_mode=True)
-        self.h2_PT_hd3_spatial = ResSDNLayer(filters[1], self.CatChannels, 16, range(4), 3, 1, 1, upsample)
-        self.h2_PT_hd3_bn = nn.BatchNorm2d(self.CatChannels)
-        self.h2_PT_hd3_relu = nn.ReLU(inplace=True)
+        self.h2_PT_hd3_conv = dilated_block(filters[1], self.CatChannels)
 
         # h3->80*80, hd3->80*80, Concatenation
         self.h3_Cat_hd3_conv = dilated_block(filters[2], self.CatChannels)
@@ -109,7 +105,7 @@ class UNet_3Plus_Spatial_Dilated(nn.Module):
         self.hd5_UT_hd3_conv = dilated_block(filters[4], self.CatChannels)
 
         # fusion(h1_PT_hd3, h2_PT_hd3, h3_Cat_hd3, hd4_UT_hd3, hd5_UT_hd3)
-        self.conv3d_1 = dilated_block(self.UpChannels, self.UpChannels)  # 16
+        self.conv3d_1 = dilated_spatial_block(self.UpChannels, self.UpChannels)  # 16
 
         '''stage 2d '''
         # h1->320*320, hd2->160*160, Pooling 2 times
@@ -254,7 +250,7 @@ class UNet_3Plus_Spatial(nn.Module):
 
         # h2->160*160, hd4->40*40, Pooling 4 times
         self.h2_PT_hd4 = nn.MaxPool2d(4, 4, ceil_mode=True)
-        self.h2_PT_hd4_spatial = ResSDNLayer(filters[1], self.CatChannels, 16, range(4), 3, 1, 1, True)
+        self.h2_PT_hd4_conv = nn.Conv2d(filters[1], self.CatChannels, 3, padding=1)
         self.h2_PT_hd4_bn = nn.BatchNorm2d(self.CatChannels)
         self.h2_PT_hd4_relu = nn.ReLU(inplace=True)
 
@@ -276,9 +272,7 @@ class UNet_3Plus_Spatial(nn.Module):
         self.hd5_UT_hd4_relu = nn.ReLU(inplace=True)
 
         # fusion(h1_PT_hd4, h2_PT_hd4, h3_PT_hd4, h4_Cat_hd4, hd5_UT_hd4)
-        self.conv4d_1 = nn.Conv2d(self.UpChannels, self.UpChannels, 3, padding=1)  # 16
-        self.bn4d_1 = nn.BatchNorm2d(self.UpChannels)
-        self.relu4d_1 = nn.ReLU(inplace=True)
+        self.spatial4d_1 = spatial_block(self.UpChannels, self.UpChannels, True)
 
         '''stage 3d'''
         # h1->320*320, hd3->80*80, Pooling 4 times
@@ -289,7 +283,7 @@ class UNet_3Plus_Spatial(nn.Module):
 
         # h2->160*160, hd3->80*80, Pooling 2 times
         self.h2_PT_hd3 = nn.MaxPool2d(2, 2, ceil_mode=True)
-        self.h2_PT_hd3_spatial = ResSDNLayer(filters[1], self.CatChannels, 16, range(4), 3, 1, 1, True)
+        self.h2_PT_hd3_conv = nn.Conv2d(filters[1], self.CatChannels, 3, padding=1)
         self.h2_PT_hd3_bn = nn.BatchNorm2d(self.CatChannels)
         self.h2_PT_hd3_relu = nn.ReLU(inplace=True)
 
@@ -311,9 +305,7 @@ class UNet_3Plus_Spatial(nn.Module):
         self.hd5_UT_hd3_relu = nn.ReLU(inplace=True)
 
         # fusion(h1_PT_hd3, h2_PT_hd3, h3_Cat_hd3, hd4_UT_hd3, hd5_UT_hd3)
-        self.conv3d_1 = nn.Conv2d(self.UpChannels, self.UpChannels, 3, padding=1)  # 16
-        self.bn3d_1 = nn.BatchNorm2d(self.UpChannels)
-        self.relu3d_1 = nn.ReLU(inplace=True)
+        self.spatial3d_1 = spatial_block(self.UpChannels, self.UpChannels, True)
 
         '''stage 2d '''
         # h1->320*320, hd2->160*160, Pooling 2 times
@@ -413,20 +405,20 @@ class UNet_3Plus_Spatial(nn.Module):
 
         ## -------------Decoder-------------
         h1_PT_hd4 = self.h1_PT_hd4_relu(self.h1_PT_hd4_bn(self.h1_PT_hd4_conv(self.h1_PT_hd4(h1))))
-        h2_PT_hd4 = self.h2_PT_hd4_relu(self.h2_PT_hd4_bn(self.h2_PT_hd4_spatial(self.h2_PT_hd4(h2))))
+        h2_PT_hd4 = self.h2_PT_hd4_relu(self.h2_PT_hd4_bn(self.h2_PT_hd4_conv(self.h2_PT_hd4(h2))))
         h3_PT_hd4 = self.h3_PT_hd4_relu(self.h3_PT_hd4_bn(self.h3_PT_hd4_conv(self.h3_PT_hd4(h3))))
         h4_Cat_hd4 = self.h4_Cat_hd4_relu(self.h4_Cat_hd4_bn(self.h4_Cat_hd4_conv(h4)))
         hd5_UT_hd4 = self.hd5_UT_hd4_relu(self.hd5_UT_hd4_bn(self.hd5_UT_hd4_conv(self.hd5_UT_hd4(hd5))))
-        hd4 = self.relu4d_1(self.bn4d_1(self.conv4d_1(
-            torch.cat((h1_PT_hd4, h2_PT_hd4, h3_PT_hd4, h4_Cat_hd4, hd5_UT_hd4), 1)))) # hd4->40*40*UpChannels
+        hd4 = self.spatial4d_1(
+            torch.cat((h1_PT_hd4, h2_PT_hd4, h3_PT_hd4, h4_Cat_hd4, hd5_UT_hd4), 1)) # hd4->40*40*UpChannels
 
         h1_PT_hd3 = self.h1_PT_hd3_relu(self.h1_PT_hd3_bn(self.h1_PT_hd3_conv(self.h1_PT_hd3(h1))))
-        h2_PT_hd3 = self.h2_PT_hd3_relu(self.h2_PT_hd3_bn(self.h2_PT_hd3_spatial(self.h2_PT_hd3(h2))))
+        h2_PT_hd3 = self.h2_PT_hd3_relu(self.h2_PT_hd3_bn(self.h2_PT_hd3_conv(self.h2_PT_hd3(h2))))
         h3_Cat_hd3 = self.h3_Cat_hd3_relu(self.h3_Cat_hd3_bn(self.h3_Cat_hd3_conv(h3)))
         hd4_UT_hd3 = self.hd4_UT_hd3_relu(self.hd4_UT_hd3_bn(self.hd4_UT_hd3_conv(self.hd4_UT_hd3(hd4))))
         hd5_UT_hd3 = self.hd5_UT_hd3_relu(self.hd5_UT_hd3_bn(self.hd5_UT_hd3_conv(self.hd5_UT_hd3(hd5))))
-        hd3 = self.relu3d_1(self.bn3d_1(self.conv3d_1(
-            torch.cat((h1_PT_hd3, h2_PT_hd3, h3_Cat_hd3, hd4_UT_hd3, hd5_UT_hd3), 1)))) # hd3->80*80*UpChannels
+        hd3 = self.spatial3d_1(
+            torch.cat((h1_PT_hd3, h2_PT_hd3, h3_Cat_hd3, hd4_UT_hd3, hd5_UT_hd3), 1)) # hd3->80*80*UpChannels
 
         h1_PT_hd2 = self.h1_PT_hd2_relu(self.h1_PT_hd2_bn(self.h1_PT_hd2_conv(self.h1_PT_hd2(h1))))
         h2_Cat_hd2 = self.h2_Cat_hd2_relu(self.h2_Cat_hd2_bn(self.h2_Cat_hd2_conv(h2)))
@@ -629,7 +621,6 @@ class UNet_3Plus_Dilated(nn.Module):
         d1 = self.outconv1(hd1)  # d1->320*320*n_classes
         
         return d1
-
 
 class UNet_3Plus(nn.Module):
 
